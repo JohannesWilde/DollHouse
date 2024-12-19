78e428d21f6aed2d92e717b3dbc771eb67493673
Johannes Wilde <git@jowil.de>
2024-12-19 06:50:55 +0100

    Move statemachine definitions to separate header for reusability.

---
+++ dollHouseStatemachine.hpp
@@ -0,0 +1,66 @@
+#ifndef DOLL_HOUSE_STATEMACHINE_HPP
+#define DOLL_HOUSE_STATEMACHINE_HPP
+
+#include <ArduinoDrivers/buttonTimed.hpp>
+
+#include <colors/colorCustom.hpp>
+#include <colors/sevenSegmentRgb.hpp>
+
+#include <helpers/statemachine.hpp>
+#include <helpers/tmpLoop.hpp>
+
+
+namespace DollHouse
+{
+
+struct DataType
+{
+    Colors::ColorCustom & settingsColor;
+    bool & saveSettings;
+    Colors::ColorCustom & displayColor;
+    bool & updateDisplay;
+    size_t const buttonIndex;
+    bool incrementBrightness = false;
+    ButtonTimedProperties::Duration_t stateTimeout = 0;
+};
+
+class StateOff : public Helpers::AbstractState<DataType>
+{
+    void init(DataType & data) const override;
+    AbstractState const & process(DataType & data) const override;
+    void deinit(DataType & data) const override;
+};
+
+extern StateOff const stateOff;
+
+class StateOn : public Helpers::AbstractState<DataType>
+{
+    void init(DataType & data) const override;
+    AbstractState const & process(DataType & data) const override;
+    void deinit(DataType & data) const override;
+};
+
+extern StateOn const stateOn;
+
+class StateBrightness : public Helpers::AbstractState<DataType>
+{
+    void init(DataType & data) const override;
+    AbstractState const & process(DataType & data) const override;
+    void deinit(DataType & data) const override;
+};
+
+extern StateBrightness const stateBrightness;
+
+class StateHue : public Helpers::AbstractState<DataType>
+{
+    void init(DataType & data) const override;
+    AbstractState const & process(DataType & data) const override;
+    void deinit(DataType & data) const override;
+};
+
+extern StateHue const stateHue;
+
+
+} // namespace DollHouse
+
+#endif // DOLL_HOUSE_STATEMACHINE_HPP
--- tests/testDollHouseStatemachine.cpp
+++ tests/testDollHouseStatemachine.cpp
@@ -1,229 +1,9 @@
-#include <colors/colorCustom.hpp>
-#include <colors/sevenSegmentRgb.hpp>
-
-#include <helpers/statemachine.hpp>
-#include <helpers/tmpLoop.hpp>
-
 #include <dollHouseButtons.hpp>
+#include <dollHouseStatemachine.hpp>
 
 #include <cassert>
-#include <cstdint>
 #include <iostream>
 #include <string.h>
-
-
-
-// Statemachine
-
-struct DataType
-{
-    Colors::ColorCustom & settingsColor;
-    bool & saveSettings;
-    Colors::ColorCustom & displayColor;
-    bool & updateDisplay;
-    size_t const buttonIndex;
-    bool incrementBrightness = false;
-    ButtonTimedProperties::Duration_t stateTimeout = 0;
-};
-
-class StateOff : public Helpers::AbstractState<DataType>
-{
-    void init(DataType & data) const override;
-    AbstractState const & process(DataType & data) const override;
-    void deinit(DataType & data) const override;
-};
-
-static StateOff const stateOff;
-
-class StateOn : public Helpers::AbstractState<DataType>
-{
-    void init(DataType & data) const override;
-    AbstractState const & process(DataType & data) const override;
-    void deinit(DataType & data) const override;
-};
-
-static StateOn const stateOn;
-
-class StateBrightness : public Helpers::AbstractState<DataType>
-{
-    void init(DataType & data) const override;
-    AbstractState const & process(DataType & data) const override;
-    void deinit(DataType & data) const override;
-};
-
-static StateBrightness const stateBrightness;
-
-class StateHue : public Helpers::AbstractState<DataType>
-{
-    void init(DataType & data) const override;
-    AbstractState const & process(DataType & data) const override;
-    void deinit(DataType & data) const override;
-};
-
-static StateHue const stateHue;
-
-
-void StateOff::init(DataType & data) const
-{
-    data.displayColor = Colors::ColorCustom(0., 0.);
-    data.updateDisplay = true;
-}
-
-Helpers::AbstractState<DataType> const & StateOff::process(DataType & data) const
-{
-    Helpers::AbstractState<DataType> const * nextState = this;
-    if (DollHouse::buttonIsSingleDownShortFinished(data.buttonIndex))
-    {
-        nextState = &stateOn;
-    }
-    else
-    {
-        // intentionally empty
-    }
-    return *nextState;
-}
-
-void StateOff::deinit(DataType & data) const
-{
-  // intentionally empty
-}
-
-
-void StateOn::init(DataType & data) const
-{
-    data.displayColor = data.settingsColor;
-    data.updateDisplay = true;
-}
-
-Helpers::AbstractState<DataType> const & StateOn::process(DataType & data) const
-{
-    Helpers::AbstractState<DataType> const * nextState = this;
-    if (DollHouse::buttonIsSingleDownShortFinished(data.buttonIndex))
-    {
-        nextState = &stateOff;
-    }
-    else if (DollHouse::buttonIsDownLong(data.buttonIndex))
-    {
-        nextState = &stateBrightness;
-    }
-    else if (DollHouse::buttonIsDoubleDownShortFinished(data.buttonIndex))
-    {
-        nextState = &stateHue;
-    }
-    else
-    {
-        // intentionally empty
-    }
-    return *nextState;
-}
-
-void StateOn::deinit(DataType & data) const
-{
-    // intentionally empty
-}
-
-
-void StateBrightness::init(DataType & data) const
-{
-    // Change direction each time.
-    data.incrementBrightness = !data.incrementBrightness;
-}
-
-Helpers::AbstractState<DataType> const & StateBrightness::process(DataType & data) const
-{
-    Helpers::AbstractState<DataType> const * nextState = this;
-    if (DollHouse::buttonIsDownLong(data.buttonIndex))
-    {
-        // As long as button stays down, modify brightness.
-        static constexpr float brightnessStep = 1./256.;
-        if (data.incrementBrightness)
-        {
-            float const newBrightness = data.displayColor.brightness + brightnessStep;
-            if (1. < newBrightness)
-            {
-                data.displayColor.brightness = 1.;
-            }
-            else
-            {
-                data.displayColor.brightness = newBrightness;
-            }
-        }
-        else
-        {
-            float const newBrightness = data.displayColor.brightness - brightnessStep;
-            if (0. > newBrightness)
-            {
-                data.displayColor.brightness = 0.;
-            }
-            else
-            {
-                data.displayColor.brightness = newBrightness;
-            }
-        }
-        data.updateDisplay = true;
-    }
-    else
-    {
-        // Fall back to stateOn once button is no longer pressed down.
-        nextState = &stateOn;
-    }
-    return *nextState;
-}
-
-void StateBrightness::deinit(DataType & data) const
-{
-    data.settingsColor = data.displayColor;
-    data.saveSettings = true;
-}
-
-
-void StateHue::init(DataType & data) const
-{
-    data.stateTimeout = DollHouse::durationStateTimeout;
-}
-
-Helpers::AbstractState<DataType> const & StateHue::process(DataType & data) const
-{
-    Helpers::AbstractState<DataType> const * nextState = this;
-    if (DollHouse::buttonIsSingleDownShortFinished(data.buttonIndex))
-    {
-        // Change to next major hue.
-        data.displayColor.hue = Colors::SevenSegmentRgb::nextMajorHue(data.displayColor.hue);
-
-        data.updateDisplay = true;
-        // Reset timeout while the user still interacts with this state.
-        data.stateTimeout = DollHouse::durationStateTimeout;
-    }
-    else if (DollHouse::buttonIsDownLong(data.buttonIndex))
-    {
-        // Change hue continuously.
-        float nextHue = data.displayColor.hue + 5.f * Colors::SevenSegmentRgb::singleDeltaHue();
-        if (1. <= nextHue)
-        {
-            nextHue -= 1.;
-        }
-        data.displayColor.hue = nextHue;
-
-        data.updateDisplay = true;
-        // Reset timeout while the user still interacts with this state.
-        data.stateTimeout = DollHouse::durationStateTimeout;
-    }
-    else if ((0 == data.stateTimeout) || DollHouse::buttonIsDoubleDownShortFinished(data.buttonIndex))
-    {
-        nextState = &stateOn;
-    }
-    else
-    {
-        --data.stateTimeout;
-    }
-    return *nextState;
-}
-
-void StateHue::deinit(DataType & data) const
-{
-    data.settingsColor = data.displayColor;
-    data.saveSettings = true;
-}
 
 
 int main(int argc, char* argv[])
@@ -247,14 +27,14 @@ int main(int argc, char* argv[])
     }
 
     // statemachine
-    DataType dataTypes[DollHouse::numberOfButtons] = {
+    DollHouse::DataType dataTypes[DollHouse::numberOfButtons] = {
         {settingsColors[0], saveSettings, displayColors[0], updateDisplay, 0, },
         {settingsColors[1], saveSettings, displayColors[1], updateDisplay, 1, },
     };
 
-    Helpers::Statemachine<DataType> statemachines[DollHouse::numberOfButtons] = {
-        Helpers::Statemachine(stateOff),
-        Helpers::Statemachine(stateOff),
+    Helpers::Statemachine<DollHouse::DataType> statemachines[DollHouse::numberOfButtons] = {
+        Helpers::Statemachine(DollHouse::stateOff),
+        Helpers::Statemachine(DollHouse::stateOff),
     };
 
     // loop
