e12be243b88665f8c3f487f3c2c9ee0657c403e7
Johannes Wilde <git@jowil.de>
2025-01-15 21:06:24 +0100

    Use dynamicaly typed buttons instead.

--- tests/testButtonTimeMultiple.cpp
+++ tests/testButtonTimeMultiple.cpp
@@ -1,7 +1,10 @@
 
 #include <ArduinoDrivers/button.hpp>
+#include <ArduinoDrivers/buttonDynamic.hpp>
+#include <ArduinoDrivers/buttonTimedProperties.hpp>
+#include <ArduinoDrivers/buttonTimedMultiple.hpp>
 #include <ArduinoDrivers/simplePinBit.hpp>
-#include <ArduinoDrivers/deprecated/buttonTimed.hpp>
+
 #include <ArduinoDrivers/deprecated/buttonTimedMultiple.hpp>
 
 #include <cassert>
@@ -23,18 +26,50 @@ typedef SimplePinBit<1, buttonsMemory> pin1;
 typedef Button<pin0, downState> button0;
 typedef Button<pin1, downState> button1;
 
-typedef TMP::ButtonTimedMultiple<button0, durationShort, durationLong, durationCombineMax> buttonTimedMultiple0;
-typedef TMP::ButtonTimedMultiple<button0, durationShort, durationLong, durationCombineMax> buttonTimedMultiple1;
+
+static void buttonInitialize(uint8_t const index)
+{
+    switch (index)
+    {
+    case 0: button0::initialize();
+    case 1: button1::initialize();
+    }
+    assert(2 > index);
+}
+
+static void buttonDeinitialize(uint8_t const index)
+{
+    switch (index)
+    {
+    case 0: button0::deinitialize();
+    case 1: button1::deinitialize();
+    }
+    assert(2 > index);
+}
+
+static bool buttonIsDown(uint8_t const index)
+{
+    switch (index)
+    {
+    case 0: return button0::isDown();
+    case 1: return button1::isDown();
+    }
+    assert(2 > index);
+    return false;
+}
+
+typedef ButtonDynamic<uint8_t, &buttonInitialize, &buttonIsDown, &buttonDeinitialize> CustomButton;
+typedef ButtonTimedMultiple<CustomButton, durationShort, durationLong, durationCombineMax, 5, uint8_t> CustomButtomTimedMultiple;
+
+static CustomButtomTimedMultiple buttonTimedMultiple0(0);
+static CustomButtomTimedMultiple buttonTimedMultiple1(1);
+
 
 int main(int argc, char* argv[])
 {
     // initialize
     static_assert(0 < sizeof(buttonsMemory));
     memset(buttonsMemory, 0, sizeof(buttonsMemory) / sizeof(buttonsMemory[0]));
-    pin0::initialize();
-    pin1::initialize();
-    buttonTimedMultiple0::initialize();
-    buttonTimedMultiple1::initialize();
 
     // button
     pin0::set(upState);
@@ -47,114 +82,108 @@ int main(int argc, char* argv[])
 
 
     // buttonTimedMultiple
-    buttonTimedMultiple0::clearHistory();
+    buttonTimedMultiple0.clearHistory();
     pin0::set(downState);
-    assert(!buttonTimedMultiple0::isDownShort());
+    assert(!buttonTimedMultiple0.isDownShort());
     for (size_t index = 0; index < durationShort; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isUp());
-    assert(buttonTimedMultiple0::isDown());
-    assert(buttonTimedMultiple0::isDownShort());
-    assert(!buttonTimedMultiple0::isDownLong());
+    assert(!buttonTimedMultiple0.isUp());
+    assert(buttonTimedMultiple0.isDown());
+    assert(buttonTimedMultiple0.isDownShort());
+    assert(!buttonTimedMultiple0.isDownLong());
     for (size_t index = 0; index < (durationLong - durationShort); ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isUp());
-    assert(buttonTimedMultiple0::isDown());
-    assert(!buttonTimedMultiple0::isDownShort());
-    assert(buttonTimedMultiple0::isDownLong());
+    assert(!buttonTimedMultiple0.isUp());
+    assert(buttonTimedMultiple0.isDown());
+    assert(!buttonTimedMultiple0.isDownShort());
+    assert(buttonTimedMultiple0.isDownLong());
 
     // double click
-    buttonTimedMultiple0::clearHistory();
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.clearHistory();
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(upState);
     for (size_t index = 0; index < durationLong; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(downState);
     for (size_t index = 0; index < durationShort; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(upState);
     for (size_t index = 0; index < durationCombineMax; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(downState);
     for (size_t index = 0; index < durationShort; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(upState);
     for (size_t index = 0; index < durationCombineMax; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     // double finished
-    buttonTimedMultiple0::update();
-    assert(buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.update();
+    assert(buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     // forget with time
-    buttonTimedMultiple0::update();
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.update();
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
 
     // single click
-    buttonTimedMultiple0::clearHistory();
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.clearHistory();
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(upState);
     for (size_t index = 0; index < durationLong; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(downState);
     for (size_t index = 0; index < durationShort; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     pin0::set(upState);
     for (size_t index = 0; index < durationCombineMax; ++index)
     {
-        buttonTimedMultiple0::update();
+        buttonTimedMultiple0.update();
     }
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
     // finished single
-    buttonTimedMultiple0::update();
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.update();
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(buttonTimedMultiple0.isSingleDownShortFinished());
     // forget with time
-    buttonTimedMultiple0::update();
-    assert(!buttonTimedMultiple0::isDoubleDownShortFinished());
-    assert(!buttonTimedMultiple0::isSingleDownShortFinished());
+    buttonTimedMultiple0.update();
+    assert(!buttonTimedMultiple0.isDoubleDownShortFinished());
+    assert(!buttonTimedMultiple0.isSingleDownShortFinished());
 
-
-    // deinitialize
-    pin0::deinitialize();
-    pin1::deinitialize();
-    buttonTimedMultiple0::deinitialize();
-    buttonTimedMultiple1::deinitialize();
 
     std::cout << "The End." << std::endl;
 }
